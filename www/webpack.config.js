389be52ad410408c9b427269f03f97ee127b4b5f
Matthieu Sprunck <matthieu@sprunck.com>
2022-02-23 20:55:54 +0100

    Add URL codec (#1)
    - Restructure code
    - Add tailwind for styles

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,21 +1,35 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./src/bootstrap.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
   mode: "development",
+  devServer: {
+    watchFiles: ["src/index.html"],
+  },
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin({
+      patterns: ["./src/index.html", { from: "static", to: "static" }],
+    }),
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
       {
-        exclude: /node_modules/
-      }
-    ]
-  }
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
 };
