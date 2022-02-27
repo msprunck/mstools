0de67ed06fa90d118a09ab696c503b04cde48bbd
msprunck <matthieu@sprunck.com>
2022-02-27 20:26:16 +0100

    Fix wasm-bindgen tests

--- tests/web.rs
+++ tests/web.rs
@@ -2,10 +2,7 @@
 
 #![cfg(target_arch = "wasm32")]
 
-extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
-
-extern crate mstools;
 use mstools::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
@@ -16,14 +13,14 @@ fn test_encode_decode_base64() {
     let encoded = encode_base64(&input);
     let decoded = decode_base64(&encoded);
     assert_eq!(&input, &decoded);
-    assert_eq!(&encoded, String::from("MTIzNA=="));
+    assert_eq!(encoded, String::from("MTIzNA=="));
 }
 
 #[wasm_bindgen_test]
-fn test_encode_decode_base64() {
+fn test_encode_decode_url() {
     let input = "http://www.example.com";
     let encoded = encode_url(&input);
     let decoded = decode_url(&encoded);
     assert_eq!(&input, &decoded);
-    assert_eq!(&encoded, String::from("http%3A%2F%2Fwww.example.com"));
+    assert_eq!(encoded, String::from("http%3A%2F%2Fwww.example.com"));
 }
